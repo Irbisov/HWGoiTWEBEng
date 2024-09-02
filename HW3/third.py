64d1f94cd858519dfd5a17f221751fcf7409fd15
Ihor Palii <paliiihor69@gmail.com>
2024-09-02 10:24:48 +0300

    Start

--- HW3/third.py
+++ HW3/third.py
@@ -1,5 +1,6 @@
-from pathlib import Path, PurePath
-import shutil
+from pathlib import Path
+from threading import Thread
+import shutil, logging, time
 
 
 def user_extensions() -> str:
@@ -18,35 +19,46 @@ def make_dr_separated_directory() -> None:
 
 def parsing_directory_extensions() -> None:
     extensions_files = user_extensions()
-    user_input_path_to_directory = input('Input to work directory : ')
+    # user_input_path_to_directory = input('Input to work directory : ')
+    user_input_path_to_directory = Path('/home/rogstrix/PycharmProjects')
     path_to_directory = Path(user_input_path_to_directory)
     if path_to_directory.exists():
         items = path_to_directory.glob('**/*')
         for item in items:
-            if item.is_file() and  item.suffix :
-                    if item.suffix in extensions_files:
-                        # # Вихідний і цільовий файли
-                        source = Path(item)
-                        destination = Path(
-                            f'/home/rogstrix/PycharmProjects/HWGoiTWEBEng/HW3/dist/{item.suffix}/{item.name}')
-                        # # Копіювання файла
-                        if source != destination:
-                            try:
-                                shutil.copy(source, destination)
-                            except Exception:
-                                continue
-                        else:
-                            print(f"Source and destination paths are the same!{source}")
-    print("Saccesfully copied..")
+            if item.is_file() and item.suffix:
+                if item.suffix in extensions_files:
+                    # # Вихідний і цільовий файли
+                    source = Path(item)
+                    destination = Path(
+                        f'/home/rogstrix/PycharmProjects/HWGoiTWEBEng/HW3/dist/{item.suffix}/{item.name}')
+                    # # Копіювання файла
+                    if source != destination:
+                        try:
+                            shutil.copy(source, destination)
+                        except Exception:
+                            continue
+                    else:
+                        logging.debug(f"Source and destination paths are the same!{source}")
+    logging.debug("Saccesfully copied..")
+    return None
+
+# path = /home/rogstrix/PycharmProjects
+
+def main() -> None:
+    print('Welcome to the file copy program!')
+    print(f'Selected extensions for filtering: -->  {user_extensions()}')
+    make_dr_separated_directory()
+    parsing_directory_extensions()
     return None
 
 
-def main():
-    print('Welcome to the file copy program!')
-    print(f'Selected extensions for filtering {user_extensions()}')
-    make_dr_separated_directory()
-    parsing_directory_extensions()
-
-
 if __name__ == "__main__":
-    main()
+    start_time = time.time()
+    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    # for i in range(5):
+    #     thread = Thread(target=main)
+    #     thread.start()
+    main()  # single-threaded execution for testing purposes (remove for production)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Час виконання програми: {execution_time:.5f} секунд")
