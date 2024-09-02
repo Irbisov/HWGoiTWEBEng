7fc442d66b0d86207cb5a45667161ba6d2ab3be8
Ihor Palii <paliiihor69@gmail.com>
2024-09-02 13:24:54 +0300

    Start5 whith Task 2 add

---
+++ HW3/Task2.py
@@ -0,0 +1,54 @@
+import time
+import multiprocessing
+
+
+def factorize(*numbers):
+    def get_factors(number):
+        factors = set()
+        for i in range(1, int(number ** 0.5) + 1):
+            if number % i == 0:
+                factors.add(i)
+                factors.add(number // i)
+        return sorted(factors)
+
+    return [get_factors(number) for number in numbers]
+
+
+# Вимірювання часу виконання синхронної версії
+start_time = time.time()
+
+a, b, c, d = factorize(128, 255, 99999, 10651060)
+
+end_time = time.time()
+print(f"Synchronous execution time: {end_time - start_time} seconds")
+
+
+def get_factors(number):
+    factors = set()
+    for i in range(1, int(number ** 0.5) + 1):
+        if number % i == 0:
+            factors.add(i)
+            factors.add(number // i)
+    return sorted(factors)
+
+
+def factorize(numbers):
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        return pool.map(get_factors, numbers)
+
+
+# Вимірювання часу виконання паралельної версії
+start_time = time.time()
+
+numbers = [128, 255, 99999, 10651060]
+a, b, c, d = factorize(numbers)
+
+end_time = time.time()
+print(f"Parallel execution time: {end_time - start_time} seconds")
+
+
+assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106,
+             1521580, 2130212, 2662765, 5325530, 10651060]
